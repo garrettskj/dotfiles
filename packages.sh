a9fc57ceaa6ed86f8b16d2df9db76354c819b3e4
Garrett Skjelstad <garrettskj@gmail.com>
2019-03-05 23:36:24 -0800

    added net-tools

--- packages.sh
+++ packages.sh
@@ -14,11 +14,11 @@ fi
 
 ## Package installation
 if [ $OS = "ubuntu" ]; then
-	sudo apt update vlc xz-utils exfat-utils xclip vim tmux deluge curl minicom irssi openssh-server -y
+	sudo apt update vlc xz-utils exfat-utils net-tools xclip vim tmux deluge curl minicom irssi openssh-server -y
 fi
 
 if [ $OS = "manjarolinux" ]; then
-	sudo pacman -Syu vim tmux xclip irssi openssh vlc-nightly weechat minicom deluge curl python-pip python2-pip intel-ucode --noconfirm
+	sudo pacman -Syu vim tmux net-tools xclip irssi openssh vlc-nightly weechat minicom deluge curl python-pip python2-pip intel-ucode --noconfirm
 	yaourt -S google-chrome-beta --noconfirm
 fi
 
