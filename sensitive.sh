244be01aaa4e12eb3cc7fe02f6dad130989a82e8
Garrett Skjelstad <garrettskj@gmail.com>
2019-03-05 22:27:01 -0800

    modified for usage explanation

--- sensitive.sh
+++ sensitive.sh
@@ -6,27 +6,32 @@
 #
 LASTPASS='/usr/bin/lpass'
 
-# Login to Lastpass and get status
-lplogin=$(exec $LASTPASS login $1)
-lpstatus=$(exec $LASTPASS status)
-
-## Check to see if Logged in
-if [[ $lpstatus =~ .*Not.* ]]
- then
-  echo $lpstatus
- else
-  echo $lpstatus
-  # If logged in...
-  # export out authorized keys
-  #
-  if [ ! -f ~/.ssh/authorized_keys ]
+if [ $# -eq 0 ]
+then
+ echo "Usage: ./sensitive.sh lastpasslogin"
+else
+ # Login to Lastpass and get status
+ lplogin=$(exec $LASTPASS login $1)
+ lpstatus=$(exec $LASTPASS status)
+ 
+ ## Check to see if Logged in
+ if [[ $lpstatus =~ .*Not.* ]]
   then
-    echo "Creating & adding authorized_keys"
-    mkdir -p ~/.ssh && touch ~/.ssh/authorized_keys
-    chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys
-    lpcopykeys=$(exec $LASTPASS show --notes authorized_keys >> ~/.ssh/authorized_keys)
+   echo $lpstatus
   else
-   echo "Already Exists! Overwriting.."
-   lpcopykeys=$(exec $LASTPASS show --notes authorized_keys > ~/.ssh/authorized_keys)
-  fi
-fi
+   echo $lpstatus
+   # If logged in...
+   # export out authorized keys
+   #
+   if [ ! -f ~/.ssh/authorized_keys ]
+   then
+     echo "Creating & adding authorized_keys"
+     mkdir -p ~/.ssh && touch ~/.ssh/authorized_keys
+     chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys
+     lpcopykeys=$(exec $LASTPASS show --notes authorized_keys >> ~/.ssh/authorized_keys)
+   else
+    echo "Already Exists! Overwriting.."
+    lpcopykeys=$(exec $LASTPASS show --notes authorized_keys > ~/.ssh/authorized_keys)
+   fi
+ fi
+fi 
