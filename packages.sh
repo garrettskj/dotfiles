393a6a82e13154db85b7d00782ec01ccaa5c741e
Garrett Skjelstad <garrettskj@gmail.com>
2019-03-06 13:46:18 -0800

    updated packages.sh with more keys

--- packages.sh
+++ packages.sh
@@ -17,7 +17,9 @@ if [ $OS = "ubuntu" ]; then
 	# Install the signing keys
 	wget -qO - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 	wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
-
+	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+	wget -q0 - https://packagecloud.io/slacktechnologies/slack/gpgkey | sudo apt-key add -
+	
 	## Update PPAs
 	# Chrome
 	sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
